50490c5c28efd57dac87a62741ab471e5d619905
LurieMrZhang <530538715@qq.com>
2015-10-14 15:12:03 +0800

    create

---
+++ src/main/java/org/lurie/App.java
@@ -0,0 +1,13 @@
+package org.lurie;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
